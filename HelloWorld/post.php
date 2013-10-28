84ea68fe18b8a5f20c58173365aba2a354e33ddc
pharonyk <basimdarwish@Basims-MacBook-Pro.local>
2013-10-28 15:45:24 -0400

    k

---
+++ HelloWorld/post.php
@@ -0,0 +1,44 @@
+<?php
+sleep(3);
+
+if (empty($_POST['email'])) {
+	$return['error'] = true;
+	$return['msg'] = 'Invalid email. Please try again.';
+}
+else {
+	$return['error'] = false;
+	$return['msg'] = 'Your email ' . $_POST['email'] . ' has been subscribed!';
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+$to      = $_POST['email'];
+$subject = 'Presentation';
+$message = 'WOW! FANTASTIC PRESENTATION, BASIM!
+A++ KUDOS!';
+$headers = 'From: MDeVito@po-box.esu.edu' . "\r\n" .
+    'Reply-To: MDeVito@po-box.esu.edu' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+	
+	
+}
+
+echo json_encode($return);
